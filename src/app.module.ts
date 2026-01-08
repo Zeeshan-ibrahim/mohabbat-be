3feed1567ccf0c469cc0a827822c4ce7b3b2e0fd
Zeeshan <zeeshanibrahim78@gmail.com>
2026-01-09 02:41:27 +0500

    Resolving build issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { ComingSoonModule } from './coming-soon/coming-soon.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    PrismaModule,
     ComingSoonModule,
     // AuthModule,
   ],
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,9 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [
-    PrismaModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
