021ed0356f2362699876090994dde3a077e5935e
Zeeshan <zeeshanibrahim78@gmail.com>
2026-01-09 04:48:30 +0500

    Enhance Prisma service by declaring global PrismaClient instance to prevent multiple initializations.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,13 @@
-// prisma.service.ts (or prisma.ts)
+// src/prisma/prisma.service.ts
 import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+// Only create a new PrismaClient if it doesn't exist
 if (!global.prisma) {
   prisma = new PrismaClient();
   global.prisma = prisma;
